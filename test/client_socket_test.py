c1e7fd905a1a4a465f7e01e4e2e6b6056c290179
AtsukiYokota <atsuki.yokota@gmail.com>
2020-02-16 19:44:12 +0900

    Fix bug

--- test/client_socket_test.py
+++ test/client_socket_test.py
@@ -1,10 +1,13 @@
 import socket
+import struct
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect(('192.168.0.100', 50000))
+    s.connect(('192.168.1.100', 50000))
     while True:
-        data = s.recv(1024)
+        data = s.recv(24)
         if not data:
             break
-        print(data)
+        print(len(data))
+        res = struct.unpack('<ddd', data)
+        print(res)
     s.close()
