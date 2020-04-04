40357546a5719049ae393c6fc49e99ad83a41d75
TKazutaka <p104068k@mail.kyutech.jp>
2020-04-04 15:44:28 +0900

    add distribute_task

--- src/cobotta_server.cpp
+++ src/cobotta_server.cpp
@@ -6,10 +6,34 @@
 
 #include <string>
 #include <vector>
+#include <map> 
 #include <unordered_map>
 #include <ros/ros.h>
 #include <tf2_ros/transform_listener.h>
 #include <geometry_msgs/TransformStamped.h>
+
+#include <iostream>
+#include <cmath>
+
+// #define PRINT(...) \
+// std::cout << "[" << __LINE__ << "] " << #__VA_ARGS__ << std::endl; \
+// __VA_ARGS__;
+
+#define PRINT(...) __VA_ARGS__
+
+  struct data
+  {
+    std::string name;
+    double distance;
+
+  };
+
+    auto operator<<(std::ostream& os, const data& datum)
+      -> std::ostream&
+    {
+      return os << "{" << datum.name << ", " << datum.distance << "}";
+    }
+
 
 template < typename T > std::string to_string( const T& n )
 {
@@ -29,6 +53,14 @@ void transform_to_array(geometry_msgs::TransformStamped transform, double* data_
   //data_array[6] = transform.transform.rotation.w;
 }
 
+double cobotta_center_array[] = {-0.35, 0.0, 0.0};
+double cobotta_right_array[] = {0.10, -0.35, 0.0};
+double cobotta_left_array[] = {0.10, 0.35, 0.0};
+
+int distance(double x,double y){
+  double distance = 0.0;
+  distance = x;
+}
 
 int main(int argc, char** argv)
 {
@@ -46,6 +78,9 @@ int main(int argc, char** argv)
 
   std::unordered_map<std::string, std::vector<geometry_msgs::TransformStamped>> object_transforms;
   std::unordered_map<std::string, std::vector<geometry_msgs::TransformStamped>> client_trasforms;
+  std::map<std::string, double > cobotta_center_to_object_distance;
+  std::map<std::string, double > cobotta_right_to_object_distance;
+  std::map<std::string, double > cobotta_left_to_object_distance;
   client_trasforms["cobotta_center"] = std::vector<geometry_msgs::TransformStamped>();
   client_trasforms["cobotta_right"] = std::vector<geometry_msgs::TransformStamped>();
   client_trasforms["cobotta_left"] = std::vector<geometry_msgs::TransformStamped>();
@@ -57,9 +92,9 @@ int main(int argc, char** argv)
 
   int sockfd, client1_sockfd, client2_sockfd, client3_sockfd;
   std::unordered_map<std::string, std::string> clients_name_info = {
-    {"192.168.1.101", "cobotta_center"},
-    {"192.168.1.102", "cobotta_right"},
-    {"192.168.1.103", "cobotta_left"},
+    {"192.168.0.103", "cobotta_center"},
+    {"192.168.0.102", "cobotta_right"},
+    {"192.168.0.101", "cobotta_left"},
   };
   std::unordered_map<std::string, int> clients_sockfd_info;
   struct sockaddr_in addr, from_addr;
@@ -145,7 +180,340 @@ int main(int argc, char** argv)
     "cobotta_center", "cobotta_right", "cobotta_left"
   };
   std::string static_object_name;
+    std::cout <<"hypot_befor" << std::endl;
   for(auto trans : object_transforms)
+  {
+    for(auto cobotta_trans : trans.second)
+    {
+      cobotta_center_to_object_distance[trans.first] 
+        = std::hypot(
+            cobotta_center_array[0] - cobotta_trans.transform.translation.x,
+            cobotta_center_array[1] - cobotta_trans.transform.translation.y);
+
+      cobotta_right_to_object_distance[trans.first]
+        = std::hypot(
+            cobotta_right_array[0] - cobotta_trans.transform.translation.x,
+            cobotta_right_array[1] -cobotta_trans.transform.translation.y);
+
+      cobotta_left_to_object_distance[trans.first] = std::hypot(cobotta_left_array[0] - cobotta_trans.transform.translation.x, cobotta_left_array[1] -cobotta_trans.transform.translation.y);
+    }
+  }
+  std::cout <<"hypot_finish" << std::endl;
+  //vectorへ変換
+  std::vector<data> center_sort {};
+  std::vector<data> right_sort  {};
+  std::vector<data> left_sort   {};
+  
+
+  for (const auto& each : cobotta_center_to_object_distance)
+  {
+    center_sort.push_back({each.first, each.second});
+    std::cerr << "center_sort: " << center_sort.back() << std::endl;;
+  }
+
+  for (const auto& each : cobotta_right_to_object_distance)
+  {
+    right_sort.push_back({each.first, each.second});
+    std::cerr << "right_sort: " << right_sort.back() << std::endl;;
+  }
+
+  for (auto itr = cobotta_left_to_object_distance.begin(); itr != cobotta_left_to_object_distance.end(); ++itr)
+  {
+    left_sort.push_back({itr->first, itr->second});
+    std::cerr << "left_sort: " << left_sort.back() << std::endl;;
+  }
+
+  // std::vector<data> center_value {cobotta_center_to_object_distance.size()};
+  // std::vector<data> right_value  {cobotta_right_to_object_distance.size()};
+  // std::vector<data> left_value   {cobotta_left_to_object_distance.size()};
+
+  std::vector<data> center_value;
+  std::vector<data> right_value;
+  std::vector<data> left_value;
+  center_value.clear();
+  right_value.clear();
+  left_value.clear();
+
+  // assert(center_value.size() == cobotta_center_to_object_distance.size());
+  // assert(right_value.size()  == cobotta_right_to_object_distance.size());
+  // assert(left_value.size()   == cobotta_left_to_object_distance.size());
+
+
+  //昇順ソート
+  sort(center_sort.begin(), center_sort.end(), [](const data &x, const data &y) { return x.distance < y.distance; }); 
+
+  sort(right_sort.begin(), right_sort.end(), [](const data &x, const data &y) { return x.distance < y.distance; });
+
+  sort(left_sort.begin(), left_sort.end(), [](const data &x, const data &y) { return x.distance < y.distance; });  
+  
+  int center_number = 0;
+  int   left_number = 0;
+  int  right_number = 0;
+  int  count_number = 0;
+
+  int count = object_number - 1;
+
+  std::cout << "object_number"<<object_number << std::endl;
+  std::cout << "count"<<count << std::endl;
+
+  std::cout <<"sort_finish" << std::endl;
+
+  std::vector<std::string> finish_object;
+  finish_object.clear();
+
+  int center_count = 0;
+  int left_count = 0;
+  int right_count = 0;
+  bool center_flag = true;
+  bool left_flag = true;
+  bool right_flag = true;
+  bool all_flag = true;
+
+  while (all_flag)
+  {
+    std::cerr << "; count\t\t; " << count << std::endl;
+    std::cerr << "; center_number\t; " << center_number << std::endl;
+    std::cerr << ";   left_number\t; " <<   left_number << std::endl;
+    std::cerr << ";  right_number\t; " <<  right_number << std::endl;
+
+    #define DISPLAY_EACH_CONTAINER_ELEMENT(CONTAINER)                         \
+    std::cerr << "; " #CONTAINER "\t; size " << CONTAINER.size() << std::endl; \
+    for (const auto& each : CONTAINER)                                        \
+    {                                                                         \
+      std::cerr << ";\t\t; " << each << std::endl;                            \
+    }
+
+    DISPLAY_EACH_CONTAINER_ELEMENT(center_sort);
+    DISPLAY_EACH_CONTAINER_ELEMENT(  left_sort);
+    DISPLAY_EACH_CONTAINER_ELEMENT( right_sort);
+
+    DISPLAY_EACH_CONTAINER_ELEMENT(center_value);
+    DISPLAY_EACH_CONTAINER_ELEMENT(  left_value);
+    DISPLAY_EACH_CONTAINER_ELEMENT( right_value);
+
+    std::cerr << ";" << std::endl;
+
+    while (center_flag)
+    {
+      if (center_count >= count)
+      {
+        center_flag = false;
+        break;
+      }
+
+      if (finish_object.size() == 0)
+      {
+        center_value.push_back(center_sort[center_count]);
+        finish_object.push_back(center_sort[center_count].name);
+        center_count += 1;
+        break;
+      }
+      else{
+        auto itr = std::find(finish_object.begin(), finish_object.end(), center_sort[center_count].name);
+        if (itr != finish_object.end())
+        {
+          center_count += 1;
+          continue;
+        }
+        else
+        {
+          center_value.push_back(center_sort[center_count]);
+          finish_object.push_back(center_sort[center_count].name);
+          center_count += 1;
+          break;
+        }
+      }
+    }
+
+    while (left_flag)
+    {
+      if (left_count >= count)
+      {
+        left_flag = false;
+        break;
+      }
+
+      if (finish_object.size() == 0)
+      {
+        left_value.push_back(left_sort[left_count]);
+        finish_object.push_back(left_sort[left_count].name);
+        left_count += 1;
+        break;
+      }
+      else
+      {
+        auto itr = std::find(finish_object.begin(), finish_object.end(), left_sort[left_count].name);
+        if (itr != finish_object.end())
+        {
+          left_count += 1;
+          continue;
+        }
+        else
+        {
+          left_value.push_back(left_sort[left_count]);
+          finish_object.push_back(left_sort[left_count].name);
+          left_count += 1;
+          break;
+        }
+      }
+    }
+
+    while (right_flag)
+    {
+      if (right_count >= count)
+      {
+        right_flag = false;
+        break;
+      }
+
+      if (finish_object.size() == 0)
+      {
+        right_value.push_back(right_sort[right_count]);
+        finish_object.push_back(right_sort[right_count].name);
+        right_count += 1;
+        break;
+      }
+      else
+      {
+        auto itr = std::find(finish_object.begin(), finish_object.end(), right_sort[right_count].name);
+        if (itr != finish_object.end())
+        {
+          right_count += 1;
+          continue;
+        }
+        else
+        {
+          right_value.push_back(right_sort[right_count]);
+          finish_object.push_back(right_sort[right_count].name);
+          right_count += 1;
+          break;
+        }
+      }
+    }
+
+    if (center_flag == false && right_flag == false && left_flag == false)
+    {
+      all_flag = false;
+      break;
+    }
+    else
+    {
+      continue;
+    }
+
+    // if (center_sort[center_number].name == left_sort[left_number].name)
+    // {
+    //   if (center_sort[center_number].distance >= left_sort[left_number].distance)
+    //   {
+    //     center_number++;
+    //   }
+    //   else
+    //   {
+    //     left_number++;
+    //   }
+
+    //   continue;
+    // }
+
+    // if(center_sort[center_number].name == right_sort[right_number].name)
+    // {
+    //   if(center_sort[center_number].distance >= right_sort[right_number].distance)
+    //   {
+    //     center_number++;
+    //   }
+    //   else
+    //   {
+    //     right_number++;
+    //   }
+
+    //   continue;
+    // }
+
+    // if (right_sort[right_number].name == left_sort[left_number].name)
+    // {
+    //   if(right_sort[right_number].distance >= left_sort[left_number].distance)
+    //   {
+    //     right_number++;
+    //   }
+    //   else
+    //   {
+    //     left_number++;
+    //   }
+
+    //   continue;
+    // }
+
+    // if (   center_sort[center_number].name != right_sort[right_number].name
+    //     && center_sort[center_number].name != left_sort[left_number].name 
+    //     && right_sort[right_number].name != left_sort[left_number].name)
+    // {
+    //   //assert(center_number < center_value.size());
+    //   //assert(left_number   < left_value.size());
+    //   //assert(right_number  < right_value.size());
+
+    //   PRINT(center_value[count_number] = center_sort[center_number]);
+    //   PRINT(left_value[count_number] = left_sort[left_number]);
+    //   PRINT(right_value[count_number] = right_sort[right_number]);
+    //   PRINT(center_number++);
+    //   PRINT(right_number++);
+    //   PRINT(left_number++);
+    //   PRINT(count_number++);
+      
+
+    //   // std::cout << "=====================" << std::endl;
+    //   // std::cout << left_sort[left_number].name << std::endl;
+    //   // std::cout << "=====================" << std::endl;
+    // }
+
+    // std::cerr << __LINE__ << std::endl;
+  }
+
+  std::cout <<"Distribution_finish" << std::endl;
+
+  for(auto left : left_value)
+  {
+    try
+    {
+      static_object_name = left.name;
+      std::cout << static_object_name << std::endl;
+      object_transform = tf_buffer.lookupTransform(robot_names[2], left.name, lookup_time, ros::Duration(timeout));
+    }
+    catch(tf2::TransformException &ex)
+    {
+      ROS_ERROR("Cannot find %s objects", static_object_name.c_str());
+    }
+    client_trasforms[robot_names[2]].push_back(object_transform);
+  }
+for(auto right : right_value)
+  {
+    try
+    {
+      static_object_name = right.name;
+      std::cout << static_object_name << std::endl;
+      object_transform = tf_buffer.lookupTransform(robot_names[1], right.name, lookup_time, ros::Duration(timeout));
+    }
+    catch(tf2::TransformException &ex)
+    {
+      ROS_ERROR("Cannot find %s objects", static_object_name.c_str());
+    }
+    client_trasforms[robot_names[1]].push_back(object_transform);
+  }
+
+for(auto center : center_value)
+  {
+    try
+    {
+      static_object_name = center.name;
+      std::cout << static_object_name << std::endl;
+      object_transform = tf_buffer.lookupTransform(robot_names[0], center.name, lookup_time, ros::Duration(timeout));
+    }
+    catch(tf2::TransformException &ex)
+    {
+      ROS_ERROR("Cannot find %s objects", static_object_name.c_str());
+    }
+    client_trasforms[robot_names[0]].push_back(object_transform);
+  }
+/*for(auto trans : object_transforms)
   {
     try
     {
@@ -155,7 +523,7 @@ int main(int argc, char** argv)
     }
     catch(tf2::TransformException &ex)
     {
-      ROS_ERROR("Cannot find %s objects", static_object_name);
+      ROS_ERROR("Cannot find %s objects", static_object_name.c_str());
     }
     client_trasforms[robot_names[robot_num]].push_back(object_transform);
     robot_num++;
@@ -163,7 +531,7 @@ int main(int argc, char** argv)
     {
       robot_num = 0;
     }
-  }
+  }*/
   // end dummy argorithm
   std::cout <<client_trasforms["cobotta_center"].size()<< std::endl;
   std::cout <<client_trasforms["cobotta_left"].size()<< std::endl;
@@ -174,11 +542,18 @@ int main(int argc, char** argv)
     for(auto trans : client_trasform.second)
     {
       //transform_to_array(trans, object_transform_data);
-      std::cout <<trans.transform.translation.x<< std::endl;
+      std::cout << "=====================" << std::endl;
+      std::cout << client_trasform.first << std::endl;
+      std::cout << clients_sockfd_info[client_trasform.first] << std::endl;
+      std::cout << trans.transform.translation.x << std::endl;
+      std::cout << trans.transform.translation.y << std::endl;
+      std::cout << trans.transform.translation.z << std::endl;
+      std::cout << "=====================" << std::endl;
       object_transform_data[0] = trans.transform.translation.x;
       object_transform_data[1] = trans.transform.translation.y;
       object_transform_data[2] = trans.transform.translation.z;
       write(clients_sockfd_info[client_trasform.first], object_transform_data, buf_size);
+      sleep(1);
     }
   }
 
